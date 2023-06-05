16f98430198bf732c571cd41fc17522b2bc7c858
Aleksandr Ievlev <a.a.ievlev@gmail.com>
2023-06-06 01:36:10 +0300

    add transform_in_title in str_func.py

--- str_func.py
+++ str_func.py
@@ -1,2 +1,10 @@
 def transform_in_upper(word):
     return word.upper()
+
+def transform_in_title(word):
+    """
+    Меняет первую букву входящего слова на заглавную
+    :param word: str
+    :return: word.title
+    """
+    return word.title()
